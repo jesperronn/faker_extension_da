e40e3df52604339b06302866cea171f9ccd63c59
Jesper Rønn-Jensen <jesperrr@gmail.com>
2008-11-14 12:37:48 +0100

    added zipcode, city names and phone number support

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 $:.unshift File.dirname(__FILE__)
 require 'faker'
-#require 'faker_da/address'
+require 'faker_da/address'
 require 'faker_da/name'   
-#require 'faker_da/phone_number'
+require 'faker_da/phone_number'
 require 'faker_da/ssn' 
 require 'faker_da/time' 
 
