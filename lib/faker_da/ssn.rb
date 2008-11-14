eceb15c8a6a18befb4855d85c51eee8e9a25fde3
Jesper Rønn-Jensen <jesperrr@gmail.com>
2008-11-14 09:23:20 +0100

    Aliasing :da_ssn as :da_cpr does not work right now (pls fix)

--- lib/faker_da/ssn.rb
+++ lib/faker_da/ssn.rb
@@ -1,8 +1,8 @@
 module Faker
   class Ssn
     class << self 
+      #FIXME alias_method :da_cpr, :da_ssn
       # Danish social security number has the format ddmmyy-xxxx (this version is without the dash)
-      alias_method :da_cpr, :da_ssn
       def da_ssn
         Faker::Time.random_date.strftime("%d%m%y") +
         Faker.numerify('####')
